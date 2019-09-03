6cab2fce62ebabee02e1ffe7eec24c4798c0b0b8
Bruno Ramires <brunormferreira@hotmail.com>
2019-09-03 11:42:17 -0300

    add: start tests api

--- swapi/src/tests/App.test.js
+++ swapi/src/tests/App.test.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from '../Main/App';
+
+import { getFilms } from '../services/Api'
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+test('Fake Test', () => {
+  expect(true).toBeTruthy();
+})
+
+describe('test API', () => {
+  it('should load films data', async () => {
+    const response = await getFilms().then(film => {
+      console.log(film.data.results)
+    })
+  })
+})
